dee93218ad70f2b51623d9131364297a48d8773d
DKBelka <72734929+DKBelka@users.noreply.github.com>
2024-03-31 20:14:10 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ tg.MainButton.color = '#2cab37';
 
 let item = "";
 
+let btn0 = document.getElementById("btn0");
 let btn1 = document.getElementById("btn1");
 let btn2 = document.getElementById("btn2");
 let btn3 = document.getElementById("btn3");
@@ -14,34 +15,59 @@ let btn4 = document.getElementById("btn4");
 let btn5 = document.getElementById("btn5");
 let btn6 = document.getElementById("btn6");
 
-btn1.addEventListener("click", function(){
+let btn10 = document.getElementById("btn10");
+let btn11 = document.getElementById("btn11");
+let btn12 = document.getElementById("btn12");
+let btn13 = document.getElementById("btn13");
+let btn14 = document.getElementById("btn14");
+let btn15 = document.getElementById("btn15");
+
+let btn20 = document.getElementById("btn20");
+let btn21 = document.getElementById("btn21");
+let btn22 = document.getElementById("btn22");
+let btn23 = document.getElementById("btn23");
+let btn24 = document.getElementById("btn24");
+let btn25 = document.getElementById("btn25");
+
+btn0.addEventListener("click", function(){
 	if (tg.MainButton.isVisible) {
 		tg.MainButton.hide();
 	}
 	else {
-		tg.MainButton.setText("Вы выбрали товар 1!");
+		tg.MainButton.setText("Вы выбрали товар: Ручка 38мм с большим логотипом!");
 		item = "1";
 		tg.MainButton.show();
 	}
 });
 
-btn2.addEventListener("click", function(){
+btn1.addEventListener("click", function(){
 	if (tg.MainButton.isVisible) {
 		tg.MainButton.hide();
 	}
 	else {
-		tg.MainButton.setText("Вы выбрали товар 2!");
-		item = "2";
+		tg.MainButton.setText("Вы выбрали товар: Ручка 25мм с большим логотипом!");
+		item = "1";
 		tg.MainButton.show();
 	}
 });
 
-btn3.addEventListener("click", function(){
+btn10.addEventListener("click", function(){
 	if (tg.MainButton.isVisible) {
 		tg.MainButton.hide();
 	}
 	else {
-		tg.MainButton.setText("Вы выбрали товар 3!");
+		tg.MainButton.setText("Вы выбрали товар: Втулка со скосом и квадратным отверстием 12*12мм внутри!");
+		item = "10";
+		tg.MainButton.show();
+	}
+});
+
+btn11.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Вы выбрали товар: Втулка под направляющую 12*12мм!");
 		item = "3";
 		tg.MainButton.show();
 	}
